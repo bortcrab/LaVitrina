81fa7ef72908d692605b42025344d0bcde6921c0
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2025-10-08 23:19:20 -0700

    Creación de migración y modelos: Chat, Mensaje, MensajeTexto y MensajeImagen

---
+++ models/chat.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Chat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Mensaje, { foreignKey: 'chatId'});
+
+      //Relación con usuarios
+    }
+  }
+  Chat.init({
+    nombre: DataTypes.STRING,
+    fechaCreacion: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Chat',
+  });
+  return Chat;
+};
