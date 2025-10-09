0176e92afb7c473da565f1a1dca44a90e129d012
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-10-08 22:03:31 -0700

    Se agregó el modelo usuraio
    se agregó el modelo y migración de usuario

---
+++ migrations/20251009050043-create-usuario.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombres: {
+        type: Sequelize.STRING
+      },
+      apellidoPaterno: {
+        type: Sequelize.STRING
+      },
+      apellidoMaterno: {
+        type: Sequelize.STRING
+      },
+      fechaNacimiento: {
+        type: Sequelize.DATEONLY
+      },
+      ciudad: {
+        type: Sequelize.STRING
+      },
+      correo: {
+        type: Sequelize.STRING
+      },
+      contrasenia: {
+        type: Sequelize.STRING
+      },
+      telefono: {
+        type: Sequelize.STRING
+      },
+      fotoPerfil: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
