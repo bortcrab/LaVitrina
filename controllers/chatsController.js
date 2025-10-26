ea69127cbee7f522d76fea3624262fb2ede9c47e
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2025-10-25 17:21:08 -0700

    Añadi lo de usuarioschatsDAO a chatsController

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,4 +1,6 @@
 const ChatDAO = require('../dataAccess/chatsDAO.js');
+const UsuarioChatsDAO = require('../dataAccess/usuarioChatsDAO.js');
+const UsuarioDAO = require('../dataAccess/usuariosDAO.js');
 const { AppError } = require('../utils/appError.js');
 
 class ChatController {
@@ -6,7 +8,7 @@ class ChatController {
         try {
             const { nombre, fechaCreacion, idPublicacion } = req.body;
 
-            if(!nombre || !fechaCreacion || !idPublicacion) {
+            if (!nombre || !fechaCreacion || !idPublicacion) {
                 return next(new AppError('Los campos nombre y fecha creación son requeridos.', 400))
             }
 
@@ -24,7 +26,7 @@ class ChatController {
             const id = req.params.id;
             const chat = await ChatDAO.obtenerChatPorId(id);
 
-            if(!chat) {
+            if (!chat) {
                 return next(new AppError('Chat no encontrado.', 404))
             }
 
@@ -58,7 +60,7 @@ class ChatController {
 
             const chatExists = await ChatDAO.obtenerChatPorId(id);
 
-            if(!chatExists) {
+            if (!chatExists) {
                 return next(new AppError('Chat no encontrado.', 404))
             }
 
@@ -68,6 +70,50 @@ class ChatController {
 
         } catch (error) {
             next(new AppError('Ocurrió un error al eliminar el chat.', 500))
+        }
+    }
+
+    static async agregarUsuarioAChat(req, res, next) {
+        try {
+            const { idChat } = req.params;
+            const { idUsuario } = req.body;
+
+            if (!idUsuario || !idChat) {
+                return next(new AppError('El idChat y idUsuario son requeridos.', 400));
+            }
+
+            const chatExiste = await ChatDAO.obtenerChatPorId(idChat);
+            const usuarioExiste = await UsuarioDAO.obtenerUsuarioPorId(idUsuario);
+
+            if (!chatExiste || !usuarioExiste) {
+                return next(new AppError('El chat o el usuario proporcionado no existe.', 404));
+            }
+
+            const nuevaRelacion = await UsuarioChatsDAO.agregarUsuarioAChat(idUsuario, idChat);
+
+            res.status(201).json(nuevaRelacion);
+
+        } catch (error) {
+            console.error(error);
+            next(new AppError('Ocurrió un error al agregar el usuario al chat.', 500));
+        }
+    }
+
+    static async eliminarUsuarioDeChat(req, res, next) {
+        try {
+            const { idChat, idUsuario } = req.params;
+
+            await UsuarioChatsDAO.eliminarUsuarioDeChat(idUsuario, idChat);
+
+            res.status(200).json({ message: 'Usuario eliminado del chat con éxito.' });
+
+        } catch (error) {
+            if (error.message === 'El usuario ya no pertenece a este chat.') {
+                return next(new AppError('El usuario especificado no pertenece a este chat.', 404));
+            }
+
+            console.error(error);
+            next(new AppError('Ocurrió un error al eliminar el usuario del chat.', 500));
         }
     }
 
