c0cab50d99391b3f724fcf88c5c421c87154a3a9
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-11-19 19:12:00 -0700

    Se agregó inciciar y registrar

--- FrontLaVitrina/script.js
+++ FrontLaVitrina/script.js
@@ -1,3 +1,9 @@
+
 import { HeaderComponent } from './src/components/header/header.js';
+import { IniciarSesionComponent } from './src/components/iniciarSesion/iniciarSesion.js';
+import { RegistrarUsuarioComponent } from './src/components/registrar/registrar.js';
 
 window.customElements.define('header-info', HeaderComponent);
+window.customElements.define('iniciar-sesion-info', IniciarSesionComponent); 
+window.customElements.define('registrar-usuario-info', RegistrarUsuarioComponent);
+
