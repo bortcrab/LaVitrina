190f94b0f7fc9f1bdc9320917e24ee8c59c36ec7
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2025-11-23 01:41:49 -0700

    pagina de reseñas terminada

--- FrontLaVitrina/src/components/resenias/resenias.component.js
+++ FrontLaVitrina/src/components/resenias/resenias.component.js
@@ -12,13 +12,36 @@ export class ReseniasComponent extends HTMLElement {
 
     #render(shadow) {
         shadow.innerHTML += `
+        <div class="container">
+            <div class="top-info">
+                <div class="user">
+                    <img src="../../assets/pedrito.png" alt="">
+                    <div class="user-info">
+                        <h1 id="username">Reseñas de Pedro</h1>
+                        <p>Observa lo que dicen los demás de <span id="name">Pedro</span></p>
+                        <h5 id="calificacion"><span class="estrella">★</span>4.9</h5>
+                    </div>
+                </div>
+                <button class="btn-escribir-resenia">Escribir reseña<span class="pencil">✎</span></button>
+            </div>
+            <div class="middle-info">
+                <h2>108 Reseñas</h2>
+                <div class="filtro-info">
+                    <h2>Ordenar por:</h2>
+                    <select name="filtro" id="filtro">
+                        <option value="reciente">Más reciente</option>
+                        <option value="antiguo">Más antiguo</option>
+                    </select>
+                </div>
+            </div>
+        </div>
 		`;
     }
 
     #agregaEstilo(shadow) {
         let link = document.createElement("link");
         link.setAttribute("rel", "stylesheet");
-        link.setAttribute("href", "./src/components/side-bar/sidebar.component.css");
+        link.setAttribute("href", "./src/components/resenias/resenias.component.css");
         shadow.appendChild(link);
     }
 }
