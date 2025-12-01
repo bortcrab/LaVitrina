f51772c065c601114d5a7483f30797710fbb6001
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2025-12-01 02:04:05 -0700

    ya suceden las pujas en tiempo real

--- LaVitrina/controllers/socketController.js
+++ LaVitrina/controllers/socketController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const MensajesDAO = require('../dataAccess/mensajesDAO.js');
+const pujasDAO = require('../dataAccess/pujasDAO.js');
 
 class SocketController {
     constructor(io) {
@@ -30,6 +31,14 @@ class SocketController {
     handleConnection(socket) {
         console.log(`Usuario conectado al socket: ID ${socket.usuario.id}`);
 
+        socket.on('realizar_puja', (data) => {
+            this.realizarPuja(socket, data);
+        });
+
+        socket.on('join_subasta', (idSubasta) => {
+            this.unirseSubasta(socket, idSubasta);
+        });
+
         socket.on('join_chat', (idChat) => {
             this.unirseSala(socket, idChat);
         });
@@ -43,6 +52,10 @@ class SocketController {
         });
     }
 
+    unirseSubasta(socket, idSubasta) {
+        socket.join(`subasta_${idSubasta}`);
+    }
+
     unirseSala(socket, idChat) {
         socket.join(`chat_${idChat}`);
     }
@@ -50,12 +63,24 @@ class SocketController {
     async enviarMensaje(socket, { idChat, texto }) {
         try {
             const mensajeGuardado = await MensajesDAO.crearMensaje(idChat, socket.usuario.id, { texto });
-            
+
             this.io.to(`chat_${idChat}`).emit('nuevo_mensaje', mensajeGuardado.toJSON());
-            
+
         } catch (error) {
             console.error("Error en socket controller:", error);
             socket.emit('error_mensaje', { error: 'No se pudo guardar el mensaje' });
+        }
+    }
+
+    async realizarPuja(socket, { idSubasta, pujaData }) {
+        try {
+            pujaData.idSubasta = idSubasta;
+
+            const puja = await pujasDAO.crearPuja(pujaData);
+            this.io.to(`subasta_${idSubasta}`).emit('nueva_puja', puja.toJSON());
+        } catch (error) {
+            console.log('Error en socket controller:', error);
+            socket.emit('error_mensaje', { error: 'No se pudo realizar la puja.' })
         }
     }
 }
